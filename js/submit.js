3461f0c16ade1f2a8822d4fcf10a5141fc82c8db
abm90 <abm90@gwu.edu>
2016-05-04 03:39:04 -0400

    Added files via upload

---
+++ js/submit.js
@@ -0,0 +1,19 @@
+var frm= $(#postform)
+var data= JSON.stringify(frm.serializeArray());
+
+
+$.ajax({
+            type: "POST",
+            url: "your url with method that accpects the data",
+            dataType: "json",
+            data: {
+                o: data
+            },
+            success: function (data) {
+               alert('Success');
+
+            },
+            error: function () {
+             alert('Error');
+            }
+        });
