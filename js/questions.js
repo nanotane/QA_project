b461c5477d7ac4b2b172b4cfac33c94d308ecfee
tsriggs <tsriggs@gwu.edu>
2016-05-04 21:35:25 -0700

    new question

--- js/questions.js
+++ js/questions.js
@@ -25,17 +25,17 @@
 					var template = "<div class='question-container'>" +
 										"<div class='question-content'>" +
 											"<div class ='votes'>" +
-												"<p id='votes'>" + questions[i].votes + "</p>" +
+												"<p id='votes'>" + questions.topTenList[i].votes + "</p>" +
 												"<h6>votes</h6>" +
 											"</div>" +
 											"<div class='question-link'>" +
-												"<h3><a href='answer.html?=" + questions[i].questionID + "' class='hyperlink'>" + questions[i].atext + "</a></h3>" +
+												"<h3><a href='answer.html?=" + questions.topTenList[i].questionID + "' class='hyperlink'>" + questions.topTenList[i].atext + "</a></h3>" +
 											"</div>" +
 											"<div class='post-info'>" +
 												"<h6> posted on </h6>" +
-												"<h6>" + questions[i].datePosted + "</h6>" +
+												"<h6>" + questions.topTenList[i].datePosted + "</h6>" +
 												"<h6> by </h6>" +
-												"<h6>" + questions[i].userID + "</h6>" +
+												"<h6>" + questions.topTenList[i].userID + "</h6>" +
 											"</div>";
 					$('.contentWrapper').append("</div></div>");
 				}
