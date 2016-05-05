0e9fc9630b0f990f1f485b1ae1ad0c9a0e278534
tsriggs <tsriggs@gwu.edu>
2016-05-04 19:34:57 -0700

    new html and js

---
+++ js/answer.js
@@ -0,0 +1,61 @@
+//model
+var data= JSON.stringify(frm.serializeArray());
+
+var url = document.location.href;
+id = url.split('=')[1];
+
+$.ajax({
+    type: "POST",
+    url: 'http://localhost/8080/questions/answer?for=' + id,
+    dataType: "JSON",
+    data: {
+        o:data
+    },
+    success: function (data) {
+        app.loadAndDisplayResults(data)  
+    },
+    error: function () {
+        alert('Error');
+    }
+})
+
+
+
+//VIEW
+var UIService = (function () {
+
+    var applyTemplate = function (template, data) {
+        return template
+            .replace(/\${userID}/g, data.userID)
+            .replace(/\${answerID}/g, data.answerID)
+            .replace(/\$atext}/g, data.atext)
+
+    };
+    var renderResult = function (results) {
+        return results.map(function (data) {
+            return applyTemplate(resultTemplate, data);
+        }).join("");
+    };
+
+
+var resultTemplate = $("#show-answer-template").html();
+
+    return {
+        displayAnswer: function (results) {
+            var rendering = renderResult(results);
+            $("#answer-list").html(rendering);
+        }
+    }
+}());
+
+//CONTROLLER
+var App = (function () {
+    return {
+        loadAndDisplayResults: function (data) {
+            promise.done(function (data) {
+                UIService.displayAnswer(results);
+            });
+        }
+    };
+}());
+
---
+++ js/show.js
@@ -0,0 +1,70 @@
+(function () {
+
+    //VIEW
+    var UIService = (function () {
+
+        var applyTemplate = function (template, data) {
+            return template
+                .replace(/\${userID}/g, data.userID)
+                .replace(/\${questionID}/g, data.questionID)
+                .replace(/\${qtext}/g, data.qtext)
+
+        };
+        var renderResult = function (results) {
+            return results.map(function (data) {
+                return applyTemplate(resultTemplate, data);
+            }).join("");
+        };
+
+
+        var resultTemplate = $("#show-question-template").html();
+
+        return {
+            displayQuestion: function (results) {
+                var rendering = renderResult(results);
+                $("#question-list").html(rendering);
+            }
+        }
+    }());
+    //MODEL
+
+    var DataService = (function () {
+        var url = document.location.href;
+        id = url.split('=')[1];
+
+
+        var SERVER = "http://localhost:8080";
+        var ENDPOINT = "/question/" + id;
+
+
+        return {
+            getQuestion: function () {
+                return $.ajax({
+                    type: "GET",
+                    url: SERVER + ENDPOINT,
+                    dataType: "json",
+                    contentType: "application/json"
+                });
+            }
+        };
+    }());
+
+
+    //CONTROLLER
+    var App = (function () {
+        return {
+            loadAndDisplayResults: function () {
+                var promise = DataService.getQuestion();
+                promise.done(function (results) {
+                    UIService.displayQuestion(results);
+                });
+            }
+        };
+    }());
+
+
+
+    $(document).ready(function () {
+        App.loadAndDisplayResults();
+    });
+}());
